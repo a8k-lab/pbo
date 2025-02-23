01ceab5c52a202eda25245fe368017c1f4b5abbc
abdul <up2dul@gmail.com>
2025-02-23 10:34:19 +0700

    chore(init): initial commit

---
+++ 1-student/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+  public static void main(String[] args) {
+    System.out.println("Nama lengkap: Abdul Malik");
+    System.out.println("Nama Panggilan: Abdul");
+    System.out.println("NIM: 103042310065");
+    System.out.println("Kelas asal: IF-47-01 PJJ");
+    System.out.println("Dosen wali: Dr. Kusuma Ayu Laksitowening, S.T., M.T.");
+    System.out.println("Kota asal: Tangerang");
+    System.out.println("Kota domisili: Bandung");
+    System.out.println("Pekerjaan: Software Developer");
+    System.out.println("Hobi: Running/Jogging");
+  }
+}
---
+++ 1-student/instructions.md
@@ -0,0 +1,13 @@
+# Tugas PBO forum week 1
+
+Buatlah video yang menjelaskan cara membuat program sederhana dalam bahasa Java berupa pencetakan identitas pribadi mahasiswa, yaitu:
+
+- Nama lengkap 
+- Nama Panggilan
+- NIM
+- Kelas asal
+- Dosen wali
+- Kota asal
+- Kota domisili
+- Pekerjaan
+- Hobi
